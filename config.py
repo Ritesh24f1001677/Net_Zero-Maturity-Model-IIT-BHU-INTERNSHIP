7b41afd7de15516bc1bd844e00bfb81972331dca
Ritesh Sharma <24f1001677@ds.study.iitm.ac.in>
2026-01-27 01:26:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
 # Format: postgresql://username:password@host:port/database
 SQLALCHEMY_DATABASE_URI = os.getenv(
     "DATABASE_URL",
-    "postgresql://carbon_user:bhu123@localhost:5432/carbon_db"
+    "postgresql://maturity_level_user:MikXlc4b9WxPlU48pJe13aofgti2Vo6S@dpg-d5rs5fvgi27c73bnh7u0-a/maturity_level"
 )
 
 # Disable track modifications to save resources
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
